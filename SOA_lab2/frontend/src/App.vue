e7cdccb787dea3f160e4cf26f12b4746d5f7dd67
PashcalE2 <74344847+PashcalE2@users.noreply.github.com>
2024-07-16 20:19:11 +0300

    .

--- SOA_lab2/frontend/src/App.vue
+++ SOA_lab2/frontend/src/App.vue
@@ -1,36 +1,46 @@
-<script setup>
+<script>
 </script>
 
 <template>
-  <div>
-    <pre>
-      <code>
-        public class MusicBand {
-        private Long id; //Поле не может быть null, Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
-        private String name; //Поле не может быть null, Строка не может быть пустой
-        private Coordinates coordinates; //Поле не может быть null
-        private java.time.LocalDateTime creationDate; //Поле не может быть null, Значение этого поля должно генерироваться автоматически
-        private long numberOfParticipants; //Значение поля должно быть больше 0
-        private long singlesCount; //Значение поля должно быть больше 0
-        private MusicGenre genre; //Поле не может быть null
-        private Album bestAlbum; //Поле не может быть null
-        }
-        public class Coordinates {
-        private int x;
-        private Integer y; //Поле не может быть null
-        }
-        public class Album {
-        private String name; //Поле не может быть null, Строка не может быть пустой
-        private Long tracks; //Поле не может быть null, Значение поля должно быть больше 0
-        private Long length; //Поле может быть null, Значение поля должно быть больше 0
-        }
-        public enum MusicGenre {
-        RAP,
-        PSYCHEDELIC_CLOUD_RAP,
-        POST_ROCK,
-        POST_PUNK,
-        BRIT_POP;
-        }
+  <div class="column">
+    <h3>Лабораторная работа #2</h3>
+
+    <h2>Вариант: 123</h2>
+
+    <p>
+    Разработать спецификацию в формате OpenAPI для набора веб-сервисов, реализующего следующую функциональность:
+    </p>
+
+    <p>
+      <b>Первый веб-сервис</b> должен осуществлять управление коллекцией объектов. В коллекции необходимо хранить объекты класса City, описание которого приведено ниже:
+    </p>
+
+    <pre><code>public class MusicBand {
+    private Long id; //Поле не может быть null, Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
+    private String name; //Поле не может быть null, Строка не может быть пустой
+    private Coordinates coordinates; //Поле не может быть null
+    private java.time.LocalDateTime creationDate; //Поле не может быть null, Значение этого поля должно генерироваться автоматически
+    private long numberOfParticipants; //Значение поля должно быть больше 0
+    private long singlesCount; //Значение поля должно быть больше 0
+    private MusicGenre genre; //Поле не может быть null
+    private Album bestAlbum; //Поле не может быть null
+}
+public class Coordinates {
+    private int x;
+    private Integer y; //Поле не может быть null
+}
+public class Album {
+    private String name; //Поле не может быть null, Строка не может быть пустой
+    private Long tracks; //Поле не может быть null, Значение поля должно быть больше 0
+    private Long length; //Поле может быть null, Значение поля должно быть больше 0
+}
+public enum MusicGenre {
+    RAP,
+    PSYCHEDELIC_CLOUD_RAP,
+    POST_ROCK,
+    POST_PUNK,
+    BRIT_POP;
+}
       </code>
     </pre>
   </div>
@@ -42,14 +52,36 @@ body {
 }
 
 body, div {
-  font-family: Arial, serif;
   text-align: center;
-  background-color: #f0f0f0;
+  background-color: #ffffff;
+  line-height: 1.5;
+  color: black;
+  font-family: "Segoe UI",serif;
+  font-size: 1rem;
 }
 
 pre {
   box-sizing: border-box;
+  overflow: auto;
+  white-space: pre;
+}
 
+h2 {
+  font-size: 1.5rem;
+  line-height: 1.2;
+  font-weight: 500;
+}
+
+h3 {
+  font-size: 1.75rem;
+  line-height: 1.2;
+  font-weight: 500;
+}
+
+p, pre, h1, h2, h3 {
+  text-align: left;
+  margin-top: 0;
+  margin-bottom: 1rem;
 }
 
 code {
